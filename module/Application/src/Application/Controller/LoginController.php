724b36090c56714bf6bd4eeddffc38bcd09515ab
fabrante <fritzabrante@gmail.com>
2013-02-08 15:38:24 +0100

    cambios para login desde front app

--- module/Application/src/Application/Controller/LoginController.php
+++ module/Application/src/Application/Controller/LoginController.php
@@ -3,14 +3,43 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Http\Client;
+
+use Application\Form\LoginForm;
 
 class LoginController extends AbstractActionController
 {
 
     public function indexAction()
     {
-        error_log("aqui");
+        $form = new LoginForm();
+
+
+
         return new ViewModel();
+    }
+
+    public function loginAction()
+    {
+        $client = new Client();
+        $client->setAdapter('Zend\Http\Client\Adapter\Curl');
+
+        $client->setUri('http://www.prueba.com/rest/login');
+        $client->setMethod('GET');
+
+        var_dump($this->getForm());
+        $client->setParameterPost($this->getRequest()->getPost()->toArray());
+
+        $response = $client->send();
+        if (!$response->isSuccess()) {
+            // report failure
+            $message = $response->getStatusCode() . ': ' . $response->getReasonPhrase();
+
+            $response = $this->getResponse();
+            $response->setContent($message);
+            return $response;
+        }
+
     }
 
 }
---
+++ module/Application/src/Application/Form/LoginForm.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Application\Form;
+
+use Zend\Form\Form;
+
+class LoginForm extends Form
+{
+
+    public function __construct($name = null)
+    {
+        // we want to ignore the name passed
+        parent::__construct('login');
+
+        $this->setAttribute('method', 'post');
+
+        $this->add(array(
+            'name' => 'userName',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Usuario',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'userPassword',
+            'attributes' => array(
+                'type'  => 'password',
+            ),
+            'options' => array(
+                'label' => 'Password',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'Iniciar',
+                'id' => 'submitbutton',
+            ),
+        ));
+
+    }
+
+}
